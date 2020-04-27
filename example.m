bd075951b0b74305b8c8cf1bb48d4d3963d53cec
JP <jppittma@jpp.com>
2020-04-27 16:48:37 -0400

    Additional example

--- example.m
+++ example.m
@@ -7,4 +7,11 @@ step_size = 1;
 analysis = @(matrix) corr(matrix');
 output = sliding_window_analysis(time_series, window, step_size, analysis);
 
+window = Rect();
+window.len = 1;
+time_series = [-10:10];
+
+basic = window.single_window(time_series, 0);
+
+
 save("output.mat");
