169bd9f67aff6672a00279b31f06b45beee34f75
Joseph Pierce Pittman <jppittma@wakehealth.edu>
2021-07-06 13:31:28 -0400

    Fixed example. Removed old code

--- example.m
+++ example.m
@@ -1,6 +1,6 @@
 import sliding_window.*
 
-time_series = [[-125:125] ; cumsum(randn(1,251))];
+time_series = [-125:125 ; cumsum(randn(1,251))];
 
 window = Mrect();
 step_size = 1;
@@ -8,10 +8,10 @@ analysis = @(matrix) corr(matrix');
 output = sliding_window_analysis(time_series, window, step_size, analysis);
 
 window = Rect();
-window.len = 1;
-time_series = [-10:10];
+window.set_half_width(1);
+time_series = -10:10;
 
-basic = window.single_window(time_series, 0);
+basic = window.index_from_center(time_series, 0);
 
 
 save("output.mat");
